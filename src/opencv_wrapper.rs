6de464bc9a7227c8f609b0ed1600ed59953432ed
Sho Mitarai <shomitarai1@gmail.com>
2022-07-26 16:56:49 +0900

    Remove termion for windows cross-compile

--- src/opencv_wrapper.rs
+++ src/opencv_wrapper.rs
@@ -1,28 +1,6 @@
 use opencv::{core::CV_8UC3, highgui, imgproc, prelude::*, videoio, Result};
-use termion::color;
 
 use crate::{IMAGE_HEIGHT, IMAGE_WIDTH};
-
-pub fn spawn_get_image_task() -> Result<()> {
-    let mut cam = get_cam()?;
-    loop {
-        let mut frame = Mat::default();
-
-        cam.read(&mut frame)?;
-
-        let compressed_frame =
-            get_compressed_frame(&frame, IMAGE_HEIGHT as i32, IMAGE_WIDTH as i32)?;
-        let image = get_image_from_frame(compressed_frame)?;
-
-        println!("{}", image);
-
-        let key = highgui::wait_key(10)?;
-        if key > 0 && key != 255 {
-            break;
-        }
-    }
-    Ok(())
-}
 
 pub fn get_vec8(cam: &mut impl VideoCaptureTrait) -> Result<Vec<u8>> {
     let mut frame = Mat::default();
@@ -56,27 +34,4 @@ pub fn get_compressed_frame(frame: &Mat, rows: i32, cols: i32) -> Result<Mat> {
     imgproc::resize(&frame, &mut compressed_frame, dsize, 0.0, 0.0, 1)?;
 
     Ok(compressed_frame)
-}
-
-fn get_image_from_frame(frame: Mat) -> Result<String> {
-    let mut image = String::new();
-
-    let frame_bytes = frame.data_bytes()?;
-    let width = frame.size()?.width as usize;
-
-    for i in (0..frame_bytes.len()).step_by(3) {
-        let b = frame_bytes[i];
-        let g = frame_bytes[i + 1];
-        let r = frame_bytes[i + 2];
-
-        let color = color::Rgb(r, g, b).fg_string();
-
-        let pixel: String = format!("{}■", color);
-
-        image.push_str(&pixel);
-        if (i) / 3 % width == 0 {
-            image.push('\n');
-        }
-    }
-    Ok(image)
 }
